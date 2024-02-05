7e2d1bfa37d177771ff2b5a8ad3e6fa8beafe2e7
Milthon Hidalgo <milthon.hidalgo@gmail.com>
2024-02-05 13:41:03 -0300

    Mejoras varias

---
+++ src/pages/posts/post-5.md
@@ -0,0 +1,13 @@
+---
+layout: ../../layouts/MarkdownPostLayout.astro
+title: Mi quinta entrada en el blog
+author: Alumno de Astro
+description: 'Esta entrada aparecerá sola!'
+image:
+  url: 'https://docs.astro.build/default-og-image.png'
+  alt: "La palabra 'astro' contra una ilustración de planetas y estrellas."
+pubDate: 2022-08-08
+tags: ['astro', 'éxitos']
+---
+
+Entrada 5 para una nueva prueba
---
+++ src/scripts/skills.js
@@ -0,0 +1,23 @@
+const identity = {
+  firstName: 'Milthon Hidalgo',
+  country: 'Chile',
+  occupation: 'Escritor técnico',
+  hobbies: ['fotografia', 'observación de aves', 'peñarol'],
+};
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Astro',
+  'Redacción de documentos',
+];
+
+const happy = true;
+const finished = false;
+const goal = 3;
+
+const skillColor = 'navy';
+const fontWeight = 'bold';
+const textCase = 'uppercase';
